7b6108a8416989ed8f8efbed73743b63e224aac6
unaped <hejoveco@gmail.com>
2025-12-06 17:24:48 +0100

    refactor: Add configuration module (src/config.py)

---
+++ temp_root/src/config.py
@@ -0,0 +1,26 @@
+import datetime
+from scipy.stats import uniform, randint
+
+CURRENT_DATE = datetime.datetime.now().strftime("%Y_%B_%d")
+DATA_GOLD_PATH = "./data/processed/train_data_gold.csv"
+DATA_VERSION = "00000"
+EXPERIMENT_NAME = CURRENT_DATE
+ARTIFACTS_DIR = "./models"
+RANDOM_STATE = 42
+TEST_SIZE = 0.15
+
+# Model hyperparameters
+XGBOOST_PARAMS = {
+    "learning_rate": uniform(1e-2, 3e-1),
+    "min_split_loss": uniform(0, 10),
+    "max_depth": randint(3, 10),
+    "subsample": uniform(0, 1),
+    "objective": ["reg:squarederror", "binary:logistic", "reg:logistic"],
+    "eval_metric": ["aucpr", "error"]
+}
+
+LR_PARAMS = {
+    'solver': ["newton-cg", "lbfgs", "liblinear", "sag", "saga"],
+    'penalty':  ["none", "l1", "l2", "elasticnet"],
+    'C' : [100, 10, 1.0, 0.1, 0.01]
+}
